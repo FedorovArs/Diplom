e5839088fb1646deb118c3a50cccaacf6efd2ece
Фёдоров Арсений Николаевич <Arseniy.Fedorov@russianpost.ru>
2020-03-02 17:18:50 +0300

    Send messages worked

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
-const http = require('http');
-
-const publicPath = path.join(__dirname, '../public');
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
-io.on('connection', () => {
-    console.log('User connected');
+io.on('connection', (socket) => {
+    console.log(`User with socketId ${socket.id} connected!`);
+
+    socket.on('sendMessage', (msg) => io.emit('addMessage', msg));
 });
 
 server.listen(port, () => {
